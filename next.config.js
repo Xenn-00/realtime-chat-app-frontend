8cc5ccc01130d246917c4787367f1962498d4847
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-01-08 21:17:24 +0700

    adding referrer policy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
