43fa88518eb1846e441e8e644c28eb4281ca1e55
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-01-12 10:07:05 +0700

    add message input

--- components/Chat/Feed/FeedWrapper.tsx
+++ components/Chat/Feed/FeedWrapper.tsx
@@ -3,6 +3,7 @@ import { Session } from "next-auth";
 import { useRouter } from "next/router";
 import React from "react";
 import MessagesHeader from "./Messages/Header";
+import MessageInput from "./Messages/Input";
 
 interface Props {
   session: Session;
@@ -22,14 +23,17 @@ function FeedWrapper({ session }: Props) {
       direction="column"
     >
       {conversationId && typeof conversationId === "string" ? (
-        <Flex
-          direction={"column"}
-          justify={"space-between"}
-          overflow={"hidden"}
-          flexGrow={1}
-        >
-          <MessagesHeader userId={userId} conversationId={conversationId} />
-        </Flex>
+        <>
+          <Flex
+            direction={"column"}
+            justify={"space-between"}
+            overflow={"hidden"}
+            flexGrow={1}
+          >
+            <MessagesHeader userId={userId} conversationId={conversationId} />
+          </Flex>
+          <MessageInput session={session} conversationId={conversationId} />
+        </>
       ) : (
         <div>No conversation selected</div>
       )}
