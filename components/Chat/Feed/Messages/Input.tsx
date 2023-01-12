98343d990040415f23fa36b2431fd4f1227c395a
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-01-12 10:07:22 +0700

    create input message field

---
+++ components/Chat/Feed/Messages/Input.tsx
@@ -0,0 +1,45 @@
+import { Box, Input, Text } from "@chakra-ui/react";
+import { Session } from "next-auth";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+
+interface MessageInputProps {
+  session: Session;
+  conversationId: string;
+}
+
+const MessageInput = ({ session, conversationId }: MessageInputProps) => {
+  const [messageBody, setMessageBody] = useState("");
+
+  const onSendMessage = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      // call sendMessage mutation
+    } catch (error: any) {
+      console.log("onSendMessage error", error);
+      toast.error(error?.message);
+    }
+  };
+
+  return (
+    <Box px={4} py={6} width={"100%"}>
+      <form onSubmit={() => {}}>
+        <Input
+          value={messageBody}
+          onChange={(e) => setMessageBody(e.target.value)}
+          size={"md"}
+          placeholder={"New Message"}
+          resize={"none"}
+          _focus={{
+            boxShadow: "none",
+            border: "1px solid",
+            borderColor: "whiteAlpha.300",
+          }}
+        />
+      </form>
+    </Box>
+  );
+};
+
+export default MessageInput;
