151fddf861debdfb76347582ae5df5a63c19c47d
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-01-21 16:26:58 +0700

    add onSendMessage

--- components/Chat/Feed/Messages/Input.tsx
+++ components/Chat/Feed/Messages/Input.tsx
@@ -1,7 +1,12 @@
+import { useMutation } from "@apollo/client";
 import { Box, Input, Text } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import { ObjectID } from "bson";
+import { SendMessageArguments } from "../../../../../backend/src/util/types";
+import MessageOperations from "../../../../graphql/operations/message";
+import { MessagesData } from "../../../../util/types";
 
 interface MessageInputProps {
   session: Session;
@@ -13,12 +18,69 @@ const MessageInput: React.FC<MessageInputProps> = ({
   conversationId,
 }) => {
   const [messageBody, setMessageBody] = useState("");
+  const [sendMessage] = useMutation<
+    { sendMessage: boolean },
+    SendMessageArguments
+  >(MessageOperations.Mutation.sendMessage);
 
   const onSendMessage = async (event: React.FormEvent) => {
     event.preventDefault();
 
     try {
       // call sendMessage mutation
+      const { id: senderId } = session.user;
+      const messageId = new ObjectID().toString();
+      const newMessage: SendMessageArguments = {
+        id: messageId,
+        senderId,
+        conversationId,
+        body: messageBody,
+      };
+
+      const { data, errors } = await sendMessage({
+        variables: {
+          ...newMessage,
+        },
+        optimisticResponse: {
+          sendMessage: true,
+        },
+        update: (cache) => {
+          const existing = cache.readQuery<MessagesData>({
+            query: MessageOperations.Query.messages,
+            variables: { conversationId },
+          }) as MessagesData;
+
+          cache.writeQuery<MessagesData, { conversationId: string }>({
+            query: MessageOperations.Query.messages,
+            variables: { conversationId },
+            data: {
+              ...existing,
+              messages: [
+                {
+                  id: messageId,
+                  body: messageBody,
+                  senderId: session.user.id,
+                  conversationId,
+                  sender: {
+                    id: session.user.id,
+                    username: session.user.username,
+                    image: session.user.image,
+                  },
+                  createdAt: new Date(Date.now()),
+                  updatedAt: new Date(Date.now()),
+                },
+                ...existing.messages,
+              ],
+            },
+          });
+        },
+      });
+
+      setMessageBody("");
+
+      if (!data?.sendMessage || errors) {
+        throw new Error("Failed to send message!");
+      }
     } catch (error: any) {
       console.log("onSendMessage error", error);
       toast.error(error?.message);
@@ -27,7 +89,7 @@ const MessageInput: React.FC<MessageInputProps> = ({
 
   return (
     <Box px={4} py={6} width={"100%"}>
-      <form onSubmit={() => {}}>
+      <form onSubmit={onSendMessage}>
         <Input
           value={messageBody}
           onChange={(e) => setMessageBody(e.target.value)}
