2aa06221add59d547caa0f1263a02e3d5fd052a6
Xenon-00 <faqihfirmansyah350@gmail.com>
2023-01-21 16:28:03 +0700

    add subscribeToMoreMessages

--- components/Chat/Feed/Messages/Messages.tsx
+++ components/Chat/Feed/Messages/Messages.tsx
@@ -1,8 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { Flex, Stack } from "@chakra-ui/react";
-import { MessagesData, MessagesVariable } from "../../../../util/types";
+import {
+  MessagesData,
+  MessageSubscriptionData,
+  MessagesVariable,
+} from "../../../../util/types";
 import MessageOperations from "../../../../graphql/operations/message";
 import { toast } from "react-hot-toast";
+import SkeletonLoader from "../../../common/SkeletonLoader";
+import MessageItem from "./MessageItem";
+import { useEffect } from "react";
+import user from "../../../../graphql/operations/user";
 
 interface MessagesProps {
   userId: string;
@@ -20,18 +28,49 @@ const Messages: React.FC<MessagesProps> = ({ userId, conversationId }) => {
     },
   });
 
-  console.log("HERE IS MESSAGES DATA", data);
+  const subscribeToMoreMessages = (conversationId: string) => {
+    subscribeToMore({
+      document: MessageOperations.Subscription.messageSent,
+      variables: {
+        conversationId,
+      },
+      updateQuery: (prev, { subscriptionData }: MessageSubscriptionData) => {
+        if (!subscriptionData) return prev;
+
+        const newMessage = subscriptionData.data.messageSent;
+        return Object.assign({}, prev, {
+          messages:
+            newMessage.sender.id === userId
+              ? prev.messages
+              : [newMessage, ...prev.messages],
+        });
+      },
+    });
+  };
+
+  useEffect(() => {
+    subscribeToMoreMessages(conversationId);
+  }, [conversationId]);
+
+  if (error) {
+    return null;
+  }
+
   return (
     <Flex direction={"column"} justify={"flex-end"} overflow={"hidden"}>
       {loading && (
-        <Stack>
-          <span>Loading Messages</span>
+        <Stack spacing={4} px={4}>
+          <SkeletonLoader count={4} height="50px" />
         </Stack>
       )}
       {data?.messages && (
         <Flex direction={"column-reverse"} overflowY="scroll" height={"100%"}>
           {data.messages.map((message) => (
-            <div key={message.body}>{message.body}</div>
+            <MessageItem
+              key={message.id}
+              message={message}
+              sentByMe={message.sender.id === userId}
+            />
           ))}
         </Flex>
       )}
